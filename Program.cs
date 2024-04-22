3c457451a27ca7a1a56991f2c432b6e9ab79c103
Aswini-J <aswinij@saveetha.ac.in>
2024-04-22 14:00:05 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using System;
+namespace Basic_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  
+        }
+    }
+    //Creating Base Class
+    class Tyre
+    {
+        protected void TyreType()
+        {
+  
+        }
+    }
+    //Creating Child Class
+    class Scooter : Tyre
+    {
+        public void ScooterType()
+        {
+          
+    }
+    //Creating Child Class
+    class Car : Tyre
+    {
+        public void CarType()
+        {
+            
+        }
+    }
+}
